b4f698eb73f1974654f64d04770447a8b8b963d8
Roberto Ramos <it.robramos@gmail.com>
2020-01-18 05:20:22 -0600

    delete to duplicate references

--- resources/views/office/index.blade.php
+++ resources/views/office/index.blade.php
@@ -26,11 +26,6 @@
     <link href="dashboard/assets/img/brand/favicon.png" rel="icon" type="image/png">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-    <!-- Icons -->
-    <link href="{{ URL::asset('/dashboard/assets/js/plugins/nucleo/css/nucleo.css') }}" rel="stylesheet" />
-    <link href="{{ URL::asset('/dashboard/assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet" />
-    <!-- CSS Files -->
-    <link href="{{ URL::asset('/dashboard/assets/css/argon-dashboard.css?v=1.1.0') }}" rel="stylesheet" />
 
     <!-- Icons -->
     <link href="{{ URL::asset('/dashboard/assets/js/plugins/nucleo/css/nucleo.css') }}" rel="stylesheet" />

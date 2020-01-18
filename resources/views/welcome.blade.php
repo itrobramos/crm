9fbd509517ed6c2f39061572e0a5298dc244ef4e
Roberto Ramos <it.robramos@gmail.com>
2020-01-18 04:17:13 -0600

    test assets routs on css files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,18 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Adrian Hernandez</title>
 
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="/css/magnific-popup.css">
-    <link rel="stylesheet" href="/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/css/themify-icons.css">
-    <link rel="stylesheet" href="/css/nice-select.css">
-    <link rel="stylesheet" href="/css/flaticon.css">
-    <link rel="stylesheet" href="/css/gijgo.css">
-    <link rel="stylesheet" href="/css/animate.min.css">
-    <link rel="stylesheet" href="/css/slick.css">
-    <link rel="stylesheet" href="/css/slicknav.css">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="{{URL::asset('/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/nice-select.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/gijgo.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/slick.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/slicknav.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/style.css')}}">
 </head>
 
 <body>

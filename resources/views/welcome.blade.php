e1a49d9794b6a0e44f4d79800c3396ccc507dcc8
Roberto Ramos <it.robramos@gmail.com>
2020-01-18 04:23:54 -0600

    test assets routs on js files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -346,33 +346,33 @@
     <!--/ footer  -->
 
     <!-- JS here -->
-    <script src="./js/vendor/modernizr-3.5.0.min.js"></script>
-    <script src="./js/vendor/jquery-1.12.4.min.js"></script>
-    <script src="./js/popper.min.js"></script>
-    <script src="./js/bootstrap.min.js"></script>
-    <script src="./js/owl.carousel.min.js"></script>
-    <script src="./js/isotope.pkgd.min.js"></script>
-    <script src="./js/ajax-form.js"></script>
-    <script src="./js/waypoints.min.js"></script>
-    <script src="./js/jquery.counterup.min.js"></script>
-    <script src="./js/imagesloaded.pkgd.min.js"></script>
-    <script src="./js/scrollIt.js"></script>
-    <script src="./js/jquery.scrollUp.min.js"></script>
-    <script src="./js/wow.min.js"></script>
-    <script src="./js/nice-select.min.js"></script>
-    <script src="./js/jquery.slicknav.min.js"></script>
-    <script src="./js/jquery.magnific-popup.min.js"></script>
-    <script src="./js/plugins.js"></script>
-    <script src="./js/gijgo.min.js"></script>
+    <script src="{{URL::asset('/js/vendor/modernizr-3.5.0.min.js')}}"></script>
+    <script src="{{URL::asset('/js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <script src="{{URL::asset('/js/popper.min.js')}}"></script>
+    <script src="{{URL::asset('/js/bootstrap.min.js')}}"></script>
+    <script src="{{URL::asset('/js/owl.carousel.min.js')}}"></script>
+    <script src="{{URL::asset('/js/isotope.pkgd.min.js')}}"></script>
+    <script src="{{URL::asset('/js/ajax-form.js')}}"></script>
+    <script src="{{URL::asset('/js/waypoints.min.js')}}"></script>
+    <script src="{{URL::asset('/js/jquery.counterup.min.js')}}"></script>
+    <script src="{{URL::asset('/js/imagesloaded.pkgd.min.js')}}"></script>
+    <script src="{{URL::asset('/js/scrollIt.js')}}"></script>
+    <script src="{{URL::asset('/js/jquery.scrollUp.min.js')}}"></script>
+    <script src="{{URL::asset('/js/wow.min.js')}}"></script>
+    <script src="{{URL::asset('/js/nice-select.min.js')}}"></script>
+    <script src="{{URL::asset('/js/jquery.slicknav.min.js')}}"></script>
+    <script src="{{URL::asset('/js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{URL::asset('/js/plugins.js')}}"></script>
+    <script src="{{URL::asset('/js/gijgo.min.js')}}"></script>
 
     <!--contact js-->
-    <script src="./js/contact.js"></script>
-    <script src="./js/jquery.ajaxchimp.min.js"></script>
-    <script src="./js/jquery.form.js"></script>
-    <script src="./js/jquery.validate.min.js"></script>
-    <script src="./js/mail-script.js"></script>
+    <script src="{{URL::asset('/js/contact.js')}}"></script>
+    <script src="{{URL::asset('/js/jquery.ajaxchimp.min.js')}}"></script>
+    <script src="{{URL::asset('/js/jquery.form.js')}}"></script>
+    <script src="{{URL::asset('/js/jquery.validate.min.js')}}"></script>
+    <script src="{{URL::asset('/js/mail-script.js')}}"></script>
 
-    <script src="./js/main.js"></script>
+    <script src="{{URL::asset('/js/main.js')}}"></script>
 
 
 </body>
